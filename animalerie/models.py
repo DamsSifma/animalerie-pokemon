9534839357f5950e111444da96c9ed57ccf5f089
dmassif <damien.massif@gmail.com>
2023-11-12 14:04:19 +0100

    ajoute attribut taille max lieu

--- animalerie/models.py
+++ animalerie/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Equipement(models.Model):
     id_equip = models.CharField(max_length=100, primary_key=True)
+    taille_max = models.IntegerField(default=1)
     disponibilite = models.CharField(max_length=20)
     photo = models.CharField(max_length=200)
     def __str__(self):
@@ -12,7 +13,7 @@ class Character(models.Model):
     etat = models.CharField(max_length=20)
     sexe = models.CharField(max_length=20)
     type = models.CharField(max_length=20)
-    photo = models.CharField(max_length=200)
+    photo = models.CharField(max_length=200)    
     lieu = models.ForeignKey(Equipement, on_delete=models.CASCADE)
     def __str__(self):
         return self.id_character
