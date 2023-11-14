6acfd0cbe5a65f854d0af6b2ce8cdd269fc11410
dmassif <damien.massif@gmail.com>
2023-11-14 10:31:55 +0100

    + de condition sur les lieux

--- animalerie/views.py
+++ animalerie/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import MoveForm
 from .models import Equipement, Character
+from random import randint
 
 def character_list(request):
     pokemons = Character.objects.all()
@@ -16,13 +17,53 @@ def character_detail(request, id_character):
         if form.is_valid():
             form.save(commit="False")
             nouveau_lieu = get_object_or_404(Equipement, id_equip=character.lieu.id_equip)
+            if nouveau_lieu == ancien_lieu:
+                message = f"{character.id_character} est déjà dans ce lieu"
+                return render(request, 'animalerie/character_detail.html', {'character': character, 'lieu': character.lieu, 'form': form, 'message': message, 'characters_dans_lieu': characters_dans_lieu})
+            print("nouveau_lieu : ", nouveau_lieu)
+
             if nouveau_lieu.disponibilite == "libre":
+                print("nouveau lieu est bien libre")
                 nombre_lieu = Character.objects.filter(lieu=nouveau_lieu).count()
-                if nombre_lieu > nouveau_lieu.taille_max - 1: # On regarde si le lieu se rempli
+                print(character.etat != "repus")
+                print("arene", nouveau_lieu == "arene")
+
+                # Si il n'est pas en état d'aller dans l'arène
+                if nouveau_lieu.id_equip == "arene" and character.etat != "repus":
+                    print("n'est pas en état d'aller dans l'arène")
+                    message = f"{character.id_character} n'est pas en état pour aller dans l'arène"
+                    return render(request, 'animalerie/character_detail.html', {'character': character, 'lieu': character.lieu, 'form': form, 'message': message, 'characters_dans_lieu': characters_dans_lieu})
+                
+                # Il va aller dans le nouveau lieu
+                if nombre_lieu > nouveau_lieu.taille_max - 1: # On regarde si le lieu se rempli (en comptant le nouveau personnage)
+                    print("on est dans le if")
                     nouveau_lieu.disponibilite = "occupé"
                 nouveau_lieu.save()
                 ancien_lieu.disponibilite = "libre"
                 ancien_lieu.save()
+
+                # si il est repus et dans l'arene
+                print("nouveau_lieu : " + nouveau_lieu.id_equip)
+                if nouveau_lieu.id_equip == "arene" and character.etat == "repus":
+                    if randint(0, 1):
+                        character.etat = "fatigué"
+                    else:
+                        character.etat = "blessé"
+                    character.save()
+
+                # si il est fatigué et dans le nid
+                elif nouveau_lieu.id_equip == "nid" and character.etat == "fatigué":
+                    character.etat = "affamé"
+                    character.save()
+
+                # si il est blessé et dans l'infirmerie
+                elif nouveau_lieu.id_equip == "infirmerie" and character.etat == "blessé":
+                    character.etat = "affamé"
+                    character.save()
+                elif nouveau_lieu.id_equip == "mangeoire" and character.etat == "affamé":
+                    character.etat = "repus"
+                    character.save()
+
             else:
                 character.lieu = ancien_lieu
                 character.save()
